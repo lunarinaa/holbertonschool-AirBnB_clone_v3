07373cfd42a2dba54db571391a93890cc904f762
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-03-25 17:25:55 +0400

    hot fix

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,7 +4,7 @@
 --   Grants all privileges for hbnb_test on hbnb_test_db.
 --   Grants SELECT privilege for hbnb_test on performance_schema.
 
-REATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
