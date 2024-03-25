ae867071d7458106ccb8ee7989cd44c6224f5039
lunarinaa <aydan.akbarova@gmail.com>
2024-03-25 22:50:43 +0400

    city class relationship with place

--- models/city.py
+++ models/city.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from sqlalchemy import Column, ForeignKey, String
-
+from sqlalchemy.orm import relationship
 from models.base_model import Base, BaseModel
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    
+
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade="delete", backref="city")
