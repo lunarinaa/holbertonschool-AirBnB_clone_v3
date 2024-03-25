f850b72832ac745e7e78a992fa819f8b3f292082
lunarinaa <aydan.akbarova@gmail.com>
2024-03-25 22:40:59 +0400

    User class relationship with Place

--- models/user.py
+++ models/user.py
@@ -11,3 +11,5 @@ class User(BaseModel):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    places = relationship("Place", cascade="delete", backref="user")
