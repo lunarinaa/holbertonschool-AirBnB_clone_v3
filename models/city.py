e7f27b238d6c2132db6391f06b3fed12f41e93e2
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-03-25 23:58:08 +0400

    typo fix

--- models/city.py
+++ models/city.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
 
 from models.base_model import Base, BaseModel
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    
+    """The city class, contains state ID and name"""
+
+    __tablename__ = "cities"
+
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
