819fb264a7be72a811c9c5f7259cb6bcda63a81a
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-03-27 19:02:46 +0400

    updated db engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,36 +2,36 @@ import os
 import sys
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import scoped_session, sessionmaker
 
-from models.base_model import Base, BaseModel
+from models.base_model import Base
 from models.city import City
 from models.state import State
+from models.user import User
 
-
-USER = os.getenv('HBNB_MYSQL_USER')
-PASSWORD = os.getenv('HBNB_MYSQL_PWD')
-HOST = os.getenv('HBNB_MYSQL_HOST')
-DB = os.getenv('HBNB_MYSQL_DB')
+USER = os.getenv("HBNB_MYSQL_USER")
+PASSWORD = os.getenv("HBNB_MYSQL_PWD")
+HOST = os.getenv("HBNB_MYSQL_HOST")
+DB = os.getenv("HBNB_MYSQL_DB")
 
 
 class DBStorage:
     __engine = None
     __session = None
-    
+
     def __init__(self):
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(USER, PASSWORD,
-                                              HOST, DB),
-                                      pool_pre_ping=True)
-        if 'test' in sys.argv:
+        self.__engine = create_engine(
+            "mysql+mysqldb://{}:{}@{}/{}".format(USER, PASSWORD, HOST, DB),
+            pool_pre_ping=True,
+        )
+        if "test" in sys.argv:
             Base.metadata.drop_all(self.__engine)
-    
-    def all(self, cls=None):        
+
+    def all(self, cls=None):
         if cls is None:
             objs = self.__session.query(State).all()
             objs.extend(self.__session.query(City).all())
-            # objs.extend(self.__session.query(User).all())
+            objs.extend(self.__session.query(User).all())
             # objs.extend(self.__session.query(Place).all())
             # objs.extend(self.__session.query(Review).all())
             # objs.extend(self.__session.query(Amenity).all())
@@ -40,22 +40,22 @@ class DBStorage:
                 cls = eval(cls)
             objs = self.__session.query(cls)
         return {"{}.{}".format(type(obj).__name__, obj.id): obj for obj in objs}
-    
+
     def new(self, obj):
         self.__session.add(obj)
-    
+
     def save(self):
         self.__session.commit()
-    
+
     def delete(self, obj=None):
         if obj:
             self.__session.delete(obj)
-    
+
     def reload(self):
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
-    
+
     def close(self):
         self.__session.close()
