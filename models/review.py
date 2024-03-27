27ccb796975d974ed33f79756b0926a984c998c1
lunarinaa <aydan.akbarova@gmail.com>
2024-03-27 20:21:23 +0400

    review, place classes updated

--- models/review.py
+++ models/review.py
@@ -2,12 +2,11 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-
-# Not implemented yet
-# class Review(BaseModel, Base):
-#     """ Review class to store review information """
-#     __tablename__ = "reviews"
-#     place_id = Column(String(60), nullable=False, ForeignKey("places.id"))
-#     user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
-#     text = Column(String(60), nullable=False)
+class Review(BaseModel, Base):
+    """Review class to store review information """
+    __tablename__ = "reviews"
+    place_id: Mapped[str] = mapped_column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id: Mapped[str] = mapped_column(String(60), ForeignKey("users.id"), nullable=False)
+    text: Mapped[str] = mapped_column(String(1024), nullable=False)
