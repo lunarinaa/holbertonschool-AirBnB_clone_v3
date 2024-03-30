792184f84e260a963006680745d33ebccb83e033
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-03-30 16:03:19 +0400

    created test for base model and added dosc for base model

--- models/base_model.py
+++ models/base_model.py
@@ -10,14 +10,25 @@ Base = declarative_base()
 
 
 class BaseModel:
+    """A base class for all hbnb models"""
+
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, default=datetime.now(), nullable=False)
     updated_at = Column(DateTime, default=datetime.now(), nullable=False)
 
-    """A base class for all hbnb models"""
-
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel."""
+        """Initialize a new BaseModel.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Attributes:
+            id (str): The unique identifier of the instance.
+            created_at (datetime): The datetime when the instance was created.
+            updated_at (datetime): The datetime when the instance was
+            last updated.
+        """
 
         self.id = str(uuid.uuid4())
         self.created_at = self.updated_at = datetime.now()
@@ -29,12 +40,21 @@ class BaseModel:
                     setattr(self, key, value)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
+        """Returns a string representation of the instance.
+
+        Returns:
+            str: The string representation of the instance.
+        """
         cls = (str(type(self)).split(".")[-1]).split("'")[0]
         return "[{}] ({}) {}".format(cls, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
+        """Updates updated_at with current time when instance is changed.
+
+        This method updates the `updated_at` attribute with the
+        current datetime
+        and saves the instance to the storage.
+        """
         from models import storage
 
         self.updated_at = datetime.now()
@@ -42,7 +62,11 @@ class BaseModel:
         storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
+        """Convert instance into dict format.
+
+        Returns:
+            dict: The instance represented as a dictionary.
+        """
         dictionary = self.__dict__.copy()
         dictionary["__class__"] = str(type(self).__name__)
         dictionary["created_at"] = self.created_at.isoformat()
@@ -51,12 +75,20 @@ class BaseModel:
         return dictionary
 
     def delete(self):
-        """Delete the current instance from the storage"""
+        """Delete the current instance from the storage.
+
+        This method deletes the current instance from the storage.
+        """
         from models import storage
 
         storage.delete(self)
 
     def __str__(self):
+        """Returns a string representation of the instance.
+
+        Returns:
+            str: The string representation of the instance.
+        """
         result = self.__dict__.copy()
         result.pop("_sa_instance_state", None)
         return "[{}] ({}) {}".format(type(self).__name__, self.id, result)
