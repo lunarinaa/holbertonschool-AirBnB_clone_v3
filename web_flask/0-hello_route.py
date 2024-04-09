92d511fd91ec5d96c1f770d5f9e962ac2bb51746
lunarinaa <aydan.akbarova@gmail.com>
2024-04-09 15:29:40 +0400

    starting flask web app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+from flask import Flask
+"""Starting a Flask web application"""
+
+app = Flask(__name__)
+"""object of Flask class"""
+
+@app.route('/', strict_slashes=False)
+def display():
+    """method to display hello """
+    return 'Hello HBNB!'
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port=5100)
+
