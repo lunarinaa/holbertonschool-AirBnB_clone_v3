e7a63ac6f8aad7d8b4f807adb43c57530ecbacd4
lunarinaa <aydan.akbarova@gmail.com>
2024-04-09 15:40:48 +0400

    formatted due to pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,11 +4,12 @@ from flask import Flask
 app = Flask(__name__)
 """object of Flask class"""
 
+
 @app.route('/', strict_slashes=False)
 def display():
     """method to display hello """
     return 'Hello HBNB!'
 
-if __name__=="__main__":
-    app.run(host='0.0.0.0', port=5100)
 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
