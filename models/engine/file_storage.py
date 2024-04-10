1076046215e722ae8b0bf0ce1565fd034f9841b9
lunarinaa <aydan.akbarova@gmail.com>
2024-04-10 14:54:04 +0400

    updates according to task 7

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -67,3 +67,6 @@ class FileStorage:
                     self.all()[key] = classes[val["__class__"]](**val)
         except FileNotFoundError:
             pass
+    def close(self):
+        """calls reload method for deserializing the JSON"""
+        self.reload()
--- models/state.py
+++ models/state.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from models.base_model import Base, BaseModel
 from models.city import City
+from os import getenv
+import models
 
 
 class State(BaseModel, Base):
@@ -15,3 +17,13 @@ class State(BaseModel, Base):
     name: Mapped[str] = mapped_column(String(128), nullable=False)
     cities: Mapped["City"] = relationship("City", backref="state",
                                           cascade="delete")
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Gets the list of cities linked to the current State"""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
+
