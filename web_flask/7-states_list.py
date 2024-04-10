36db725b72095cf1113ee1fc7b3f2b13c96ebd4a
lunarinaa <aydan.akbarova@gmail.com>
2024-04-10 22:53:03 +0400

    list of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from flask import Flask
+from models import storage
+from flask import render_template
+
+from models.state import State
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def display():
+    states = storage.all(State)
+    return render_template("7-states_list.html", states=states)
+
+@app.teardown_appcontext
+def teardown(exc):
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5100)
