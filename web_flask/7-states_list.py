840ed94e68ba149c3ba3445a9c4b8b3b4901f4ed
lunarinaa <aydan.akbarova@gmail.com>
2024-04-10 22:56:43 +0400

    docstrings added

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Starting web application"""
 from flask import Flask
 from models import storage
 from flask import render_template
@@ -9,12 +10,14 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def display():
+    """Displays an HTML page"""
     states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
 @app.teardown_appcontext
 def teardown(exc):
+    """Removes the current SQLalchemy session"""
     storage.close()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5100)
+    app.run(host="0.0.0.0", port=5000)
