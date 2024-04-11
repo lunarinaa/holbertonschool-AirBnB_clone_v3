cbb470603c63ebbac770c3114c94dcd12b23b279
lunarinaa <aydan.akbarova@gmail.com>
2024-04-11 14:32:26 +0400

    comments updated

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Starting web application"""
 from flask import Flask
 from models import storage
 from flask import render_template
@@ -12,12 +13,14 @@ app = Flask(__name__)
 
 @app.route('/cities_by_states', strict_slashes=False)
 def display():
+    """Displays an HTML page"""
     states = storage.all(State)
     return render_template('8-cities_by_states.html', states=states)
 
 @app.teardown_appcontext
 def teardown(exc):
+    """Removes the current SQLalchemy session"""
     storage.close()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5100)
+    app.run(host='0.0.0.0', port=5000)
