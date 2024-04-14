60ee3d51690a9683c7383c475831d24041380ef6
lunarinaa <aydan.akbarova@gmail.com>
2024-04-14 20:01:38 +0400

    documented

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Starting web application"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -8,11 +9,13 @@ app = Flask(__name__)
 
 @app.route('/states', strict_slashes=False)
 def display():
+    """Display method"""
     states = storage.all(State)
     return render_template('9-states.html', states=states)
 
 @app.route('/states/<id>', strict_slashes=False)
 def display_state(id):
+    """Display method"""
     state = storage.all(State).get(id)
     if state:
         return render_template('9-states.html', state=state)
