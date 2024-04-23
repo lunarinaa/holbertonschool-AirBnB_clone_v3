0dc08a84766a853947da59556ea0e25ddfe25963
lunarinaa <aydan.akbarova@gmail.com>
2024-04-23 22:39:54 +0400

    create city

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -2,7 +2,7 @@
 """cities"""
 
 from api.v1.views import app_views
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 from models import storage
 from models.city import City
 from models.state import State
@@ -35,3 +35,20 @@ def delete_city(city_id):
     storage.delete(the_city)
     storage.save()
     return jsonify({}), 200
+
+@app_views.route('states/<state_id>/cities', methods=['POST'])
+def create_city(state_id):
+    """Creates a City"""
+    data = request.get_json()
+    the_state = storage.get(State, state_id)
+    if the_state is None:
+        abort(404)
+    if not data:
+        abort(400, 'Not a JSON')
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    new_city = City(name=data['name'], state_id=state_id)
+    storage.new(new_city)
+    storage.save()
+    return jsonify(new_city.to_dict()), 201
+
