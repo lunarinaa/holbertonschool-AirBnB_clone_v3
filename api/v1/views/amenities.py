c7a4e02afbbc579458ae7c5bdcba2487a4431663
lunarinaa <aydan.akbarova@gmail.com>
2024-04-24 21:08:11 +0400

    delete amenity

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -13,6 +13,7 @@ def get_amenities():
     amenity = storage.all(Amenity).values()
     return jsonify([amenity.to_dict() for unit in amenity])
 
+# need to be revised
 @app_views.route('/amenities/<amenity_id>', methods=['GET'])
 def get_amenity(amenity_id):
     """Retrieves a Amenity object"""
@@ -20,3 +21,13 @@ def get_amenity(amenity_id):
     if amenity is None:
         abort(404)
     return jsonify(amenity.to_dict())
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+def delete_amenity(amenity_id):
+    """Deletes a Amenity object:"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    storage.delete(amenity)
+    storage.save()
+    return jsonify({}), 200
