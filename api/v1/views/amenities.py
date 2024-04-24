d78629aef3d20d2bb1359325489cd0f69baa036e
lunarinaa <aydan.akbarova@gmail.com>
2024-04-24 20:56:23 +0400

    uncommented

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -6,16 +6,17 @@ from flask import jsonify, abort, request
 from models import storage
 from models.amenity import Amenity
 
+# 3/25
 @app_views.route('/amenities', methods=['GET'])
 def get_amenities():
     """Retrieves the list of all Amenity objects"""
     amenity = storage.all(Amenity).values()
     return jsonify([amenity.to_dict() for unit in amenity])
 
-# @app_views.route('/amenities/<amenity_id>', methods=['GET'])
-# def get_amenity(amenity_id):
-#     """Retrieves a Amenity object"""
-#     amenity = storage.get(Amenity, amenity_id)
-#     if amenity is None:
-#         abort(404)
-#     return jsonify(amenity.to_dict())
+@app_views.route('/amenities/<amenity_id>', methods=['GET'])
+def get_amenity(amenity_id):
+    """Retrieves a Amenity object"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    return jsonify(amenity.to_dict())
