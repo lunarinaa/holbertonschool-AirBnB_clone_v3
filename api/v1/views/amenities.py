353081ddd6b483fc198842e26ffe1d76876d8f5b
lunarinaa <aydan.akbarova@gmail.com>
2024-04-25 19:24:34 +0400

    shorter logic implemented

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -18,12 +18,9 @@ def get_amenities():
 @app_views.route('/amenities/<amenity_id>', methods=['GET'])
 def get_amenity(amenity_id):
     """Retrieves a Amenity object"""
-    amenities = storage.all("Amenity").values()
-    for amenity in amenities:
-        if amenity.id == amenity_id:
-            return jsonify(amenity.to_dict())
-        else:
-            abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
     return jsonify(amenity.to_dict())
 
 
