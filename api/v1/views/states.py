0afcfe69b6c61a868f04e351ed667c454822bad6
lunarinaa <aydan.akbarova@gmail.com>
2024-04-25 15:57:04 +0400

    create state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -41,18 +41,32 @@ def delete_state(state_id):
 
 
 @app_views.route('/states/', methods=['POST'])
+# def create_state():
+#     """Creates a a new State"""
+#     if not request.get_json():
+#         abort(400, 'Not a JSON')
+#     if 'name' not in request.get_json():
+#         abort(400, 'Missing name')
+#     states = []
+#     new_state = State(name=request.json['name'])
+#     storage.new(new_state)
+#     storage.save()
+#     states.append(new_state.to_dict())
+#     return jsonify(states[0]), 201
 def create_state():
     """Creates a a new State"""
-    if not request.get_json():
+    data = request.get_json()
+    if data is None:
         abort(400, 'Not a JSON')
-    if 'name' not in request.get_json():
+    if 'name' not in data:
         abort(400, 'Missing name')
-    states = []
-    new_state = State(name=request.json['name'])
+    new_state = State(name=data['name'])
     storage.new(new_state)
     storage.save()
-    states.append(new_state.to_dict())
-    return jsonify(states[0]), 201
+    return jsonify(new_state.to_dict()), 201
+         
+
+
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'])
