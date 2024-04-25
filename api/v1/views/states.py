6829dc637aedc9178bfb0205a6b985ad555abdda
lunarinaa <aydan.akbarova@gmail.com>
2024-04-25 16:33:47 +0400

    pep fix

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -40,19 +40,6 @@ def delete_state(state_id):
     return jsonify({}), 200
 
 
-# @app_views.route('/states/', methods=['POST'])
-# def create_state():
-#     """Creates a a new State"""
-#     data = request.get_json()
-#     if data is None:
-#         abort(400, 'Not a JSON')
-#     if 'name' not in data:
-#         abort(400, 'Missing name')
-#     new_state = State(name=data['name'])
-#     storage.new(new_state)
-#     storage.save()
-#     return jsonify(new_state.to_dict()), 201
-         
 @app_views.route('/states/', methods=['POST'])
 def create_state():
     """Creates a new State"""
@@ -71,24 +58,6 @@ def create_state():
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
-
-
-# @app_views.route('/states/<state_id>', methods=['PUT'])
-# def updates_state(state_id):
-#     """Updates a State object"""
-#     states = storage.all("State").values()
-#     state_object = [object.to_dict() for object in states
-#                     if object.id == state_id]
-#     if state_object == []:
-#         abort(404)
-#     if not request.get_json():
-#         abort(400, 'Not a JSON')
-#     state_object[0]['name'] = request.json['name']
-#     for object in states:
-#         if object.id == state_id:
-#             object.name = request.json['name']
-#     storage.save()
-#     return jsonify(state_object[0]), 200
 
 @app_views.route('/states/<state_id>', methods=['PUT'])
 def updates_state(state_id):
